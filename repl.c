886858719877f5f115834f5a469dee6e31ec6540
Prakhar Srivastav <prakhar1989@gmail.com>
2016-08-17 12:34:09 -0400

    moved to linenoise instead of readline

--- repl.c
+++ repl.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "linenoise.h"
 
 int main() {
     puts("Lispy version 0.0.1");
     puts("Press Ctrl+C to exit\n");
 
+    // load history at startup
+    linenoiseHistoryLoad("history.txt");
+
+    char* line;
+
     // never ending loop
-    while (1) {
-        char* line = linenoise("lispy> ");
-        printf("You entered: %s\n", line);
+    while ((line = linenoise("lispy> ")) != NULL) {
+
+        // valid string?
+        if (line[0] != '\0' && line[0] != '/') {
+            // save to history
+            linenoiseHistoryAdd(line);
+            linenoiseHistorySave("history.txt");
+
+            // echo back the response
+            printf("You entered: %s\n", line);
+        }
+
+        // the typed string is returned as a malloc()
+        // hence needs to be freed manually
         free(line);
     }
 
