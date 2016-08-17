33779877553d55b7f9471cbc674f1769f2ba42dc
Prakhar Srivastav <prakhar1989@gmail.com>
2016-08-17 13:41:24 -0400

    added mpc and parsing ability

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all: lisp
 
 lisp: repl.c
-	$(CC) -std=c99 -Os -g -Wall -o lisp linenoise.c repl.c
+	$(CC) -std=c99 -Os -g -Wall -o lisp.out linenoise.c mpc.c repl.c
 
 clean:
-	rm lisp
+	rm *.out
+	rm -rf *.dSYM
--- repl.c
+++ repl.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "linenoise.h"
+#include "mpc.h"
 
 int main() {
+    // parsers
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Program = mpc_new("program");
+
+    // defining the grammar
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                       \
+        number   : /-?[0-9]+/ ;                             \
+        operator : '+' | '-' | '*' | '/' ;                  \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+        program  : /^/ '(' <operator> <expr>+ ')' /$/ ;     \
+    ",
+    Number, Operator, Expr, Program);
+
+    // intro message
     puts("Lispy version 0.0.1");
     puts("Press Ctrl+C to exit\n");
 
@@ -10,6 +28,7 @@ int main() {
     linenoiseHistoryLoad("history.txt");
 
     char* line;
+    mpc_result_t r;
 
     // never ending loop
     while ((line = linenoise("lispy> ")) != NULL) {
@@ -20,14 +39,25 @@ int main() {
             linenoiseHistoryAdd(line);
             linenoiseHistorySave("history.txt");
 
-            // echo back the response
-            printf("You entered: %s\n", line);
+            // parse input
+            if (mpc_parse("<stdin>", line, Program, &r)) {
+                // print AST
+                mpc_ast_print(r.output);
+                mpc_ast_delete(r.output);
+            } else {
+                // print the error
+                mpc_err_print(r.error);
+                mpc_err_delete(r.error);
+            }
         }
 
         // the typed string is returned as a malloc()
         // hence needs to be freed manually
         free(line);
     }
+
+    // cleanup our parsers
+    mpc_cleanup(4, Number, Operator, Expr, Program);
 
     return 0;
 }
